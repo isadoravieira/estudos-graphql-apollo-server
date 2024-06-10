b7fbe0bc7288642307b4651c9f0e2c804175c903
Isadora Vieira <isavieiracamara@gmail.com>
2024-06-09 23:19:57 -0300

    feat: add first typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+import { ApolloServer, gql } from "apollo-server";
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'Hello Again';
+      },
+      hi: () => {
+        return 'Hi honey';
+      },
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
+
